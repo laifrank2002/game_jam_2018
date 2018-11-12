571559f52c94b39cd1955404be000acd8a83f346
clocks-in-a-cooler <>
2018-11-11 20:23:23 -0500

    crude physics

--- explore/player_ship.js
+++ explore/player_ship.js
@@ -1,14 +1,21 @@
 var Player_ship = (function() {
-    var health = 150;
+    var HEALTH = 150;
     
-    var pos = {
+    var POS = {
         x: 100,
         y: 100,
     };
     
-    var forward_speed = 0.25;
-    var rot_speed     = 0.003;
-    var angle         = -Math.PI / 2; //start facing...up?
+    var VECTOR = {
+        x: 0,
+        y: 0,
+    };
+    
+    var THRUST    = 0.001;
+    var MAX_SPEED = 0.5;
+    var ROT_SPEED = 0.003;
+    var FRICTION  = 0.0005;
+    var angle     = -Math.PI / 2; //start facing...up?
     
     var keys = {
         forward:   false,
@@ -16,8 +23,26 @@ var Player_ship = (function() {
         rot_right: false,
     };
     
-    var ship_width_offset  = 15;
-    var ship_height_offset = 10;
+    var wrap = function() {
+        if (POS.x < 0) {
+            POS.x += EE.game_area_x;
+        }
+        
+        if (POS.x > EE.game_area_x) {
+            POS.x = POS.x % EE.game_area_x;
+        }
+        
+        if (POS.y < 0) {
+            POS.y += EE.game_area_y;
+        }
+        
+        if (POS.y > EE.game_area_y) {
+            POS.y = POS.y % EE.game_area_y;
+        }
+    };
+    
+    var SHIP_WIDTH_OFFSET  = 15;
+    var SHIP_HEIGHT_OFFSET = 10;
     
     var in_orbit  = false;
     var in_combat = false;
@@ -32,26 +57,39 @@ var Player_ship = (function() {
         
         get_new_position(lapse) {
             //first, update the angles
-            angle += (keys.rot_left ? -rot_speed * lapse : 0);
-            angle += (keys.rot_right ? rot_speed * lapse : 0);
+            angle += (keys.rot_left ? -ROT_SPEED * lapse : 0);
+            angle += (keys.rot_right ? ROT_SPEED * lapse : 0);
             
             if (keys.forward) {
-                pos.x += Math.cos(angle) * forward_speed * lapse;
-                pos.y += Math.sin(angle) * forward_speed * lapse;
+                VECTOR.x += Math.cos(angle) * THRUST * lapse;
+                VECTOR.y += Math.sin(angle) * THRUST * lapse;
+                
+                //VECTOR.x = closer_to_zero(VECTOR.x, Math.cos(angle) * MAX_SPEED);
+                //VECTOR.y = closer_to_zero(VECTOR.y, Math.sin(angle) * MAX_SPEED);
             }
+            
+            VECTOR.x = toward(VECTOR.x, FRICTION * lapse, 0);
+            VECTOR.y = toward(VECTOR.y, FRICTION * lapse, 0);
+            
+            POS.x += VECTOR.x * lapse;
+            POS.y += VECTOR.y * lapse;
+            
+            wrap();
         },
         
         draw: function(context) {
             context.save();
             
-            context.translate(pos.x, pos.y);
+            context.translate(POS.x, POS.y);
             context.rotate(angle);
-            context.drawImage(EA.player_ship, -ship_width_offset, -ship_height_offset);
+            context.drawImage(EA.player_ship, -SHIP_WIDTH_OFFSET, -SHIP_HEIGHT_OFFSET);
             
             context.restore();
         },
         
         get is_moving() { return !(in_orbit && in_combat);},
+        
+        get angle() { return angle; },
         
         get weapons() { return weapons; },
         get abilities() { return abilities; },
