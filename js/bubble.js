51000aa50828be78d12d0f1916add738a10baaf7
clocks-in-a-cooler <>
2018-11-18 19:14:13 -0500

    did some bubble work.

--- js/bubble.js
+++ js/bubble.js
@@ -8,13 +8,12 @@ function Bubble(x, y, vx, vy) {
 
     this.radius = Math.random() * 5;
 
-    this.lifetime = 0;
-    this.active   = true;
+    this.lifetime     = 0;
+    this.max_lifetime = 500 + Math.random() * 2000;
+    this.active       = true;
 }
 
 Bubble.prototype.speed = 0.1;
-
-Bubble.prototype.max_lifetime = 1500;
 
 Bubble.prototype.get_new_position = function(lapse) {
     if (this.lifetime + lapse > this.max_lifetime) {
