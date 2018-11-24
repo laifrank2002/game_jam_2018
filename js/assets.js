bedede7f06c1f37718c99d2260707c1467d1c0e1
clocks-in-a-cooler <>
2018-11-24 16:28:37 -0500

    updated the assets registry

--- js/assets.js
+++ js/assets.js
@@ -3,6 +3,9 @@ var Assets = (function() {
     var arrow       = create_image("assets/arrow.png");
     var player_ship = create_image("assets/player_ship.png");
     
+    var player_planet  = create_image("assets/player_planet.png");
+    var alien_planet_1 = create_image("assets/alien_planet_1.png");
+    
     var asteroid = create_image("assets/asteroid.png");
     var bullet   = create_image("assets/bullet.png");
     var pickup   = create_image("assets/pickupable.png");
@@ -14,6 +17,9 @@ var Assets = (function() {
         get asteroid() { return asteroid; },
         get bullet() { return bullet; },
         get pickupable() { return pickup; },
+        
+        get player_planet() { return player_planet; },
+        get alien_planet_1() { return alien_planet_1; },
     };
 })();
 
